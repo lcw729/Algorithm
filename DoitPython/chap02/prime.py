d45cedf3fe0750fcdcc7956cc63753b51ce581f4
lcw729 <lcw729729@gmail.com>
2021-03-17 21:12:06 +0900

    shallow copy and deep copy

--- DoitPython/chap02/prime.py
+++ DoitPython/chap02/prime.py
@@ -41,17 +41,20 @@ print(f'나눗셈을 실행한 횟수: {counter}') #15620
 # step 3
 
 counter = 0
-x = [2]
-ptr = 1
+x = [2, 3]
+ptr = 2
 
-for n in range(3, 1001, 2): # 홀수만을 대상으로 설정
-    for i in range(1, ptr): # 이미 찾은 소수로 나눔
-        counter += 1
+for n in range(5, 1001, 2): # 홀수만을 대상으로 설정
+    i = 1
+    while x[i]**2 <= n: 
+        counter += 2
         if n % x[i] == 0: # 나누어 떨어지면 소수가 아님
             break
+        i += 1
     else:
         x.append(n) # 소수라면 배열 x에 추가하기
         ptr += 1
+        counter += 1
         
 for i in range(ptr):
     print(x[i])
