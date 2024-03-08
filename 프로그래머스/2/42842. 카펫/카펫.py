6cd2923c8f99b808827f21a572cfbd0d9ad795e3
chaewon <48678849+lcw729@users.noreply.github.com>
2024-03-08 17:31:41 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,12 @@
+def solution(brown, yellow):
+    answer = []
+    
+    total = brown + yellow
+    for num in range(3, int(total ** 1/2) + 1):
+        if (total % num == 0):
+            w = max(num, (total / num))
+            h = total / w
+            if ((w-2) * (h-2) == yellow):
+                answer = [w, h]
+                break;
+    return answer
