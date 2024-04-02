193b2ae2e9de64c71f7f9629087d89c9aef083ab
chaewon <48678849+lcw729@users.noreply.github.com>
2024-04-02 20:50:47 +0900

    [level 2] Title: 귤 고르기, Time: 28.29 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,16 @@
+from collections import Counter
+
 def solution(k, tangerine):
     answer = 0
-    sizeBox = dict()
-    for item in tangerine:
-        if item not in sizeBox: sizeBox[item] = 1
-        else: sizeBox[item] += 1
     
-    sortedBox = sorted(sizeBox.items(), key=lambda x: x[1], reverse=True)
+    sortedBox = Counter(tangerine).most_common()
+    
+#     sortedBox = sorted(sizeBox.items(), key=lambda x: x[1], reverse=True)
     
     total = 0
     for key, value in sortedBox:
-        if total >= k: break;
         total += value
         answer += 1
+        if total >= k: break;
         
     return answer
