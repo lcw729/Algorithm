be9430a3b134dba458918e94b18852bd7215f609
chaewon <48678849+lcw729@users.noreply.github.com>
2024-04-19 00:57:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,29 +1,24 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     in_bridge = list()
-    complete = list()
-    
-    n = len(truck_weights)
     total = 0
-    while len(complete) < n:
+    while in_bridge or truck_weights:
                 
-        # 모든 다리의 시간 카운트
+        # 모든 트럭의 남은 거리 카운트
         deprecated = 0
         for idx, truck_info in enumerate(in_bridge):
             time, truck = truck_info
-            if time == 1:
+            if time == 1: # 거리 1칸 남은 경우 
                 deprecated += 1
-                total -= truck
-                complete.append(truck)
+                total -= truck # 트럭 제거
             else:
-                in_bridge[idx][0] = time - 1
+                in_bridge[idx][0] = time - 1 # 거리 감소
         del in_bridge[:deprecated]
                 
         if truck_weights:
             result = total + truck_weights[0] # 무게 합산
             if result <= weight and len(in_bridge) < bridge_length:
-                in_bridge.append([bridge_length, truck_weights[0]]) # 다리를 지난다.
-                truck_weights.pop(0)
+                in_bridge.append([bridge_length, truck_weights.pop(0)]) # 다리를 지난다.
                 total = result
                     
         answer += 1
