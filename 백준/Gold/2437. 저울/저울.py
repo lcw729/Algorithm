9db1a4e825ab02e8fcedf5e46d96e94052a4c59b
chaewon <48678849+lcw729@users.noreply.github.com>
2024-04-24 01:24:14 +0900

    [Gold II] Title: 저울, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.py
@@ -0,0 +1,12 @@
+n = int(input())
+lst = list(map(int, input().split(' ')))
+lst.sort()
+
+target = 1
+for weight in lst:
+    if target < weight:
+        break
+
+    target += weight
+
+print(target)
